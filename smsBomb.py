807bbaa1e493f0efc94aa048a4c3d392ecf7958d
maxik7898 <maximtomck96@icloud.com>
2025-04-06 15:18:00 +0300

    Update smsBomb.py

--- smsBomb.py
+++ smsBomb.py
@@ -17,10 +17,7 @@ def main():
     start = str(input("Введите [1] для запуска: "))
     while True:
         if start == "1":
-            print('Капча: Путин хуйло') # если ты залез это убрать то иди НАХУЙ!
-            caphaa = str(input("Введите капчу: "))
-
-        if start == "путин хуйло":
+            
 
             phone = input(colored('Enter your phone number>>: ', 'cyan'))
             countT = input(colored('Enter threading>>: ', 'blue'))
